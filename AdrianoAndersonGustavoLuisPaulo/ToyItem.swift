138251628a2c455e46c96ae8d3accb4a4c258705
f6365418 <luisfsena@bb.com.br>
2022-10-16 18:18:50 -0300

    Adicionando estrutura do banco de dados no arquivo ToyItem e alterando o controller da tela de boas vindas (welcome)

---
+++ AdrianoAndersonGustavoLuisPaulo/ToyItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToyItem.swift
+//  AdrianoAndersonGustavoLuisPaulo
+//
+//  Created by f6365418 on 16/10/22.
+//
+
+import Foundation
+
+struct toyItem {
+    let id: String
+    let name: String
+    let phone: String
+}
